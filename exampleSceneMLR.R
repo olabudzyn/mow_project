b11de59edc4274cc3ec72a23b731f86aefce1497
olabudzyn <ola.budz96@gmail.com>
2020-01-23 12:20:32 +0100

    refactor code, additional test for Scene

--- exampleSceneMLR.R
+++ exampleSceneMLR.R
@@ -40,12 +40,91 @@ pred = predict(mod, task = scene.task, subset = test.set)
 #names(as.data.frame(pred))
 
 
+
+#stworzenie klasyfikatora binarnego
+binary.learner = makeLearner("classif.rpart")
+
+
+###### Chains Wrapper
+lrn.cc = makeMultilabelClassifierChainsWrapper(binary.learner)
+
+# uczenie => powstaje model dla chain wrapper
+mod_cc = train(lrn.cc, scene.task, subset = train.set)
+# predykcja dla zestawu testowego
+pred_cc = predict(mod_cc, task = scene.task, subset = test.set)
+
+
+
+
+###### Nested Stacking Wrapper
+lrn.ns = makeMultilabelNestedStackingWrapper(binary.learner, order = NULL, cv.folds = 2)
+
+# uczenie => powstaje model dla nested stacking
+mod_ns = train(lrn.ns, scene.task, subset = train.set)
+# predykcja dla zestawu testowego
+pred_ns = predict(mod_ns, task = scene.task, subset = test.set)
+
+
+
 listMeasures("multilabel")
 
 # pomiary parametrów
 performance(pred, measures = list(multilabel.hamloss, multilabel.subset01, multilabel.f1, multilabel.acc))
+performance(pred_cc, measures = list(multilabel.hamloss, multilabel.subset01, multilabel.f1, multilabel.acc))
+performance(pred_ns, measures = list(multilabel.hamloss, multilabel.subset01, multilabel.f1, multilabel.acc))
+
 
 # resampling
 rdesc = makeResampleDesc("Subsample", iters = 10, split = 2/3)
 r = resample(lrn.rfsrc, scene.task, rdesc,measures = list(multilabel.hamloss, multilabel.subset01, multilabel.f1, multilabel.acc))
-r
+r_cc = resample(lrn.cc, scene.task, rdesc,measures = list(multilabel.hamloss, multilabel.subset01, multilabel.f1, multilabel.acc))
+r_ns = resample(lrn.ns, scene.task, rdesc,measures = list(multilabel.hamloss, multilabel.subset01, multilabel.f1, multilabel.acc))
+
+
+mes_values <- r[["measures.test"]]
+mes_cc_values <- r_cc[["measures.test"]]
+mes_ns_values <- r_ns[["measures.test"]]
+
+hamloss_mean = mean(mes_values[,2])
+subset_mean = mean(mes_values[,3])
+f1_mean = mean(mes_values[,4])
+acc_mean = mean(mes_values[,5])
+
+hamloss_cc_mean = mean(mes_cc_values[,2])
+subset_cc_mean = mean(mes_cc_values[,3])
+f1_cc_mean = mean(mes_cc_values[,4])
+acc_cc_mean = mean(mes_cc_values[,5])
+
+hamloss_ns_mean = mean(mes_ns_values[,2])
+subset_ns_mean = mean(mes_ns_values[,3])
+f1_ns_mean = mean(mes_ns_values[,4])
+acc_ns_mean = mean(mes_ns_values[,5])
+
+counts <- matrix(0,3,4)
+
+
+counts[1,1] = hamloss_mean
+counts[1,2] = subset_mean
+counts[1,3] = f1_mean
+counts[1,4] = acc_mean
+
+counts[2,1] = hamloss_cc_mean
+counts[2,2] = subset_cc_mean
+counts[2,3] = f1_cc_mean
+counts[2,4] = acc_cc_mean
+
+counts[3,1] = hamloss_ns_mean
+counts[3,2] = subset_ns_mean
+counts[3,3] = f1_ns_mean
+counts[3,4] = acc_ns_mean
+
+
+counts
+
+
+barplot(counts, main="Miary jakoœci",
+        xlab="Number of Gears", col=c("darkblue","red","black"),
+        names.arg=c("Hamming Loss", "Subset", "F1","Accuracy"), beside=TRUE,
+        legend = c("random forest","chain wrapper","nested stacking wrapper"))
+
+
--- main.r
+++ main.r
@@ -5,180 +5,153 @@ library("utiml")
 library("rJava")
 library("RWeka")
 library(mldr)
-## Loading required package: mldr
-## Loading required package: parallel
-## Loading required package: ROCR
-## Loading required package: gplots
-## 
-## Attaching package: 'gplots'
-## The following object is masked from 'package:stats':
-## 
-##     lowess
 
 
 myyeast <- read.arff("yeast")
-
 myscene <- read.arff("scene")
 
-hemydata_mldr <- mldr("yeast", use_xml = TRUE, auto_extension = TRUE, "yeast.xml")
-
-mydata_part <- create_holdout_partition(mydata_mldr, c(train=0.65, test=0.35), "iterative")
-
-head(myscene)
-
-d = getTaskData(myyeast.task)
 
 
-# plot data - a picture is worth a 1000 words. Melt data => then ggplot
-library(ggplot2)
-ggplot(irissubdf, aes(x = sepal, y = petal)) + 
-  geom_point(aes(colour=species, shape=species), size = 3) +
-  xlab("sepal length") + 
-  ylab("petal length") + 
-  ggtitle("Species vs sepal and petal lengths")
-# add binary labels corresponding to species - Initialize all values to 1
-# add setosa label of -1. The binary +1, -1 labels are in the fourth  
-# column. It is better to create two separate data frames: one containing
-# the attributes while the other contains the class values.
-irissubdf[, 4] <- 1
-irissubdf[irissubdf[, 3] == "setosa", 4] <- -1
+yeast <- read.arff("yeast")
+
+yeast_train <- read.arff("yeast-train",use_xml=TRUE,auto_extension = TRUE, "yeast.xml")
+yeast_test <- read.arff("yeast-test",use_xml=TRUE,auto_extension = TRUE, "yeast.xml")
+
+my_yeast_train <- yeast_train[["dataframe"]]
+yeast_train_label <- my_yeast_train[yeast_train[["labelIndices"]]]
+yeast_train_attr <- my_yeast_train[1:103]
+
+my_yeast_test <- yeast_test[["dataframe"]]
+yeast_test_label <- my_yeast_test[yeast_test[["labelIndices"]]]
+yeast_test_attr <- my_yeast_test[1:103]
 
 
 
-
-
-
-data<-read.arff("yeast")
-mydata <-data[["dataframe"]]
-
-label <-mydata[data[["labelIndices"]]]
-attr <- mydata[1:103]
-
-
-
-generateChain <- function(attr,label,eta,epoch) {
-  
-  
-  weightMatrix <- matrix(0, dim(label)[2],dim(attr)[2])
-  
-
-  for(i in 1:dim(label)[2] )  
-  {
-   weights <- perceptron(attr,label[,i], eta, epoch) 
-
-   weightMatrix[i, ] <- weights
-   
-    r <- results(weights,attr)                 
-    
-    atrr <- cbind(attr,t(r))
-  }
-  
-  
-  return(weightMatrix)
-
-}
-
-
-chain <- generateChain(attr,label,0.001,5)  
-
-result <- results(chain[14,],attr)  
 
 perceptron <- function(x, y, eta, niter) {
   
-  # initialize weight vector
- ## weight <- rep(0, dim(x)[2]+1)
-  
   errors <- rep(0, niter)
-  weight <- t(runif(dim(x)[2], min=0,max=1))
+  weight <- t(runif(dim(x)[2], min = 0, max = 1))
   
-
-  # loop over number of epochs niter
+  
   for (jj in 1:niter) {
-    
-    # loop through training data set
     for (ii in 1:length(y)) {
       
-      # Predict binary label using Heaviside activation 
-      # function
+      #sigmoidalna funkcja aktywacji
+      beta = 5
+      z <-  as.numeric(x[ii, ]) %*% t(weight)
       
-    ##  z <- sum(weight[2:length(weight)] * 
-    ##             as.numeric(x[ii, ])) + weight[1]
+      ypred = 1 / (1 + exp (-beta * z))
       
       
-    ##  if(z < 0) {
-    ##    ypred <- -1
-    ##  } else {
-    ##    ypred <- 1
-    ##  }
+      #poprawka wag
+      weightdiff <- eta * (y[ii] - ypred) *   as.numeric(x[ii,])
       
-      beta=5;
-
-
-     z <-  as.numeric(x[ii,])%*% t(weight)
-
-    ypred = 1 / ( 1 + exp ( -beta * z ) ); 
-
-      
-
-    
-      a <- y[ii] - ypred
-   
-
-      weightdiff <- eta * (y[ii] - ypred) *   as.numeric(x[ii, ])
-    
       weight <- weight + t(weightdiff)
-   
-  
-  ##     Update error function
-  ##    if ((y[ii] - ypred) != 0.0) {
-  ##      errors[jj] <- errors[jj] + 1
-  ##    }
+      
+      
+      if (abs(ypred > 0.5))
+        ypredbin = 1
+      else
+        ypredbin = 0
+      
+      #obliczanie ilosci bledow
+      if ((y[ii] - ypredbin) != 0.0) {
+        errors[jj] <- errors[jj] + 1
+      }
       
     }
   }
   
   
-
-  # weight to decide between the two species 
- 
-##print(weight)
-print(weight)
-  return(weight)
-
-
+  newList <- list(weight,errors)
+  print(newList[1])
+  return(newList)
+  
+ # return(weight)
 }
 
 
-err <- perceptron(data_attributes,label[,1], 0.001, 50)
 
 
-
-results <- function(weights,x) {
+results <- function(weights, x) {
   val <- rep(0, dim(x)[1])
   
-  for(ii in 1:dim(x)[1]){
-    
+  for (ii in 1:dim(x)[1]) {
     beta = 5
-    z <-  as.numeric(x[ii,])%*% t(weight)
+    z <-  as.numeric(x[ii, ]) %*% t(weights)
     
-    ypred = 1 / ( 1 + exp ( -beta * z ) ); 
-    
-    
+    ypred = 1 / (1 + exp (-beta * z))
     
     
-    if(abs(ypred>0.5))
+    
+    if (abs(ypred > 0.5))
       val[ii] = 1
     else
       val[ii] = 0
-       
     
   }
-    return(val)
+  return(val)
+}
+
+
+
+generateChain <- function(attr, label, eta, epoch) {
+  weightMatrix <- matrix(0, dim(label)[2], dim(attr)[2])
+  
+  
+  for (i in 1:dim(label)[2])
+  {
+    perceptron_out <- perceptron(attr, label[, i], eta, epoch)
+    
+    weights = perceptron_out[[1]]
+    
+    print(weights)
+    weightMatrix[i,] <- weights
+    
+    r <- results(weights, attr)
+    atrr <- cbind(attr, t(r))
+  }
+  
+  
+  
+  plot(1:epoch,
+       perceptron_out[[2]],
+       type = "l",
+       lwd = 2,
+       col = "red",
+       xlab = "epoch #",
+       ylab = "errors"
+  )
+  title("B³êdy - liczba epok = , wsp. uczenia = 0.01")
+  
+  
+  return(weightMatrix)
 }
 
 
 
 
+chain <- generateChain(yeast_train_attr, yeast_train_label, 0.001, 5)
+
+result <- results(chain[14, ], yeast_test_attr)
+
+
+
+
+
+
+# plot data - a picture is worth a 1000 words. Melt data => then ggplot
+library(ggplot2)
+ggplot(irissubdf, aes(x = sepal, y = petal)) +
+  geom_point(aes(colour = species, shape = species), size = 3) +
+  xlab("sepal length") +
+  ylab("petal length") +
+  ggtitle("Species vs sepal and petal lengths")
+# add binary labels corresponding to species - Initialize all values to 1
+# add setosa label of -1. The binary +1, -1 labels are in the fourth
+# column. It is better to create two separate data frames: one containing
+# the attributes while the other contains the class values.
 
 
 
@@ -190,7 +163,5 @@ results <- function(weights,x) {
 
 
 
-plot(1:10, err, type="l", lwd=2, col="red", xlab="epoch #", ylab="errors")
-title("Errors vs epoch - learning rate eta = 0.01")
 
-label[label== 0] <- -1
+
